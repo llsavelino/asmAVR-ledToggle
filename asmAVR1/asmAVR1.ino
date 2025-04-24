9a5855418ce5819fcd5beef8dcf20cea12a9b241
llsavelino <savelino852@gmail.com>
2025-04-24 14:44:21 -0300

    Update asmAVR1.ino

--- asmAVR1/asmAVR1.ino
+++ asmAVR1/asmAVR1.ino
@@ -2,7 +2,7 @@
 extern                                      "C"
 { 
   void      start                    (  void );            
-  void      led                      (uint8_t); 
+  void      led                      (  bool ); 
   void      tenLed                   (uint8_t);
   /*\       De assembly para C/C++          \*/
 }                                             ;
@@ -11,47 +11,64 @@ extern                                      "C"
 inline auto setup                 ( __empty__ ) 
 error                                   -> void            
 { 
+  Serial.               begin( 1001011000000 );
   start                                    ( );
   tenLed                              ( 0x0A );
   return                                      ;
 }
-register signed char  sta        { 0b00000000 };
+signed char                  sta{ 0b00000000 };
+#define value(_o_) (*(bool*)(void* const)& _o_) 
 inline auto loop                  ( __empty__ ) 
 error                                   -> void     
 { 
-  led        (  +(*(bool*)(void* const)& sta));   
-  led        (  !(*(bool*)(void* const)& sta)); 
+  led        (  +(value                (sta)));   
+  led        (  !(value                (sta))); 
 }
 /*\ ><><><><><><><><><><>=<><><><><><><><>< \*/
 auto main         (int argc, const char** argv) 
 noexcept(0x00) ->                decltype(0x00)
 {
-  struct {
-    using ptrFn =                void(*)(void);
-    void(*ptrSp)  (void)            { &setup };
-  	void(*ptrLp)  (void)            { &loop  };
-    /*\ ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ \*/
-    ptrFn fn            [0x02]{ ptrSp, ptrLp };
-    ptrFn                       ( *ptrArrayfn )
-    [(uint8_t)(sizeof(fn) /     sizeof(ptrFn))]         
-    {            (void* const)&fn            };
-  }
-  Ardfuncs                                    ;
-  if ( !( *(Ardfuncs.fn +0x00))              ||
-       !( *(Ardfuncs.fn +0x01))               )
-  { 
-    return                               +0x01;
-  }
-  if (   !(* (*Ardfuncs.ptrArrayfn +0x00))    )
+  try
   {
-    return                               -0x01;
-  }
+    typedef                              struct 
+    {
+      using ptrFn =              void(*)(void);
+      void(*ptrSp)  (void)          { &setup };
+  	  void(*ptrLp)  (void)          { &loop  };
+      /*\ ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ \*/
+      ptrFn fn          [0x02]{ ptrSp, ptrLp };
+      ptrFn                     ( *ptrArrayfn )
+      [(uint8_t)(sizeof(fn) /   sizeof(ptrFn))]         
+      {               (void*)&fn             };
+    }                                 Ardfuncs;
+    Ardfuncs                             _sys_;
+  
+   if (  !(_sys_.ptrArrayfn             )    ||   
+         !(* (*(_sys_.ptrArrayfn +0x00)))    ||   
+         !(* (*(_sys_.ptrArrayfn +0x01)))     )
+    {
+      throw            "Problema na mémoria\n";
+    }
+    /*\ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= \*/
+    {
+      (* (_sys_.ptrArrayfn))      [ 0x00 ](  );
+    }
+    __infiniteCycle__:
+    {
+      (* (_sys_.ptrArrayfn))      [ 0x01 ](  );
+    } goto                   __infiniteCycle__;
+  } 
+  catch                  (const char* msgError) 
   {
-    (* (Ardfuncs.ptrArrayfn))     [ 0x00 ](  );
-  }
-  __infiniteCycle__:
+    Serial.          print("Erro detectado: ");
+    Serial.                    print(msgError);
+    Serial.                        write('\n');
+    return                              -+0x01;
+  } 
+  catch                                   (...) 
   {
-    (* (Ardfuncs.ptrArrayfn))     [ 0x01 ](  );
-  } goto                     __infiniteCycle__;
+    Serial.       print("Erro desconhecido\n");
+    return                              +-0x01;
+  }
   return                                  0x00;
 };
