64c230133c3c11c4d46f680387a22a68419eb2b4
llsavelino <savelino852@gmail.com>
2025-04-29 08:14:02 -0300

    Update asmAVR1.ino

--- asmAVR1/asmAVR1.ino
+++ asmAVR1/asmAVR1.ino
@@ -1,82 +1,66 @@
-extern                                      "C"
-{ 
-  void      start                    (  void );            
-  void      led                      (  bool ); 
-  void      tenLed                   (uint8_t);
-  /*\       De assembly para C/C++          \*/
-}                                             ;
-/*\ ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ \*/
-#define        error(yesOrno) noexcept(yesOrno)
-inline auto setup                    (  void  ) 
-error(false)                            -> void            
-{ 
-  Serial.         begin( 0b11100011100000000 );
-  start                                    ( );
-  tenLed                              ( 0x0A );
-  return                                      ;
+extern "C" {
+  void start(void);
+  void led(bool);
+  void tenLed(uint8_t);
 }
-register signed char         sta{ 0b00000000 };
-#define value(i, ty, adrs)     (*(ty)(adrs) &i) 
-inline auto loop                     (  void  ) 
-error(false)                            -> void     
-{ 
-    led        (  +(value(sta, bool*, void*)));   
-    led        (  !(value(sta, bool*, void*)));
+
+#define error(yesOrno) noexcept(yesOrno)
+
+inline auto setup(void) error(false) -> void {
+  Serial.begin(0b11100011100000000);
+  start();
+  tenLed(0x0A);
 }
-auto main         (int argc, const char** argv) 
-error(false) ->                  decltype(0x00)
-{
-  try
-  {
-    typedef                              struct 
-    {
-      using ptrFn =              void(*)(void);
-      void(*ptrSp)  (void)          { &setup };
-  	  void(*ptrLp)  (void)          { &loop  };
-      /*\ ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨ \*/
-      ptrFn fn          [0x02]{ ptrSp, ptrLp };
-      ptrFn                     ( *ptrArrayfn )
-      [(uint8_t)(sizeof(fn) /   sizeof(ptrFn))]         
-      {               (void*)&fn             };
-      alignas(4) uint8_t       padding [ 0x04 ]
-      {  0x00,     0x00,     0x00,     0x00  };
-    }                                 Ardfuncs;
-    Ardfuncs                             _sys_;
-  
-   if (  !(_sys_.ptrArrayfn             )    ||   
-         !(* (*(_sys_.ptrArrayfn +0x00)))    ||   
-         !(* (*(_sys_.ptrArrayfn +0x01)))     )
-    {
-      throw            "Problema na mémoria\n";
+
+register signed char sta { 0b00000000 };
+
+inline auto loop(void) error(false) -> void {
+  led(+(*(bool*)(void*)&sta));
+  led(!(*(bool*)(void*)&sta));
+}
+
+using ptrFn = void(*)(void);
+
+typedef struct alignas(8) {
+  union alignas(8) {
+    struct {
+      alignas(8) ptrFn fn[2];
+    };
+    alignas(4) ptrFn (*ptrArrayfn)[2];
+    alignas(4) void* raw;
+  } funcStorage { { &setup, &loop } };
+
+  alignas(4) uint8_t padding[4] { 0x00, 0x00, 0x00, 0x00 };
+} Ardfuncs;
+
+static_assert(sizeof(Ardfuncs) == 16, "Erro: Ardfuncs deve ter 16 bytes!");
+
+auto main(int argc, const char** argv) error(false) -> decltype(0x00) {
+  try {
+    Ardfuncs _sys_;
+
+    if (!_sys_.funcStorage.ptrArrayfn ||
+        !(*(*_sys_.funcStorage.ptrArrayfn + 0x00)) ||
+        !(*(*_sys_.funcStorage.ptrArrayfn + 0x01))) {
+      throw "Problema na mémoria\n";
     }
-    /*\ -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= \*/
-    {
-      (* (_sys_.ptrArrayfn))      [ 0x00 ](  );
-    }
-    Serial.                            end(  );
+
+    (*(_sys_.funcStorage.ptrArrayfn))[0]();
+    Serial.end();
+
     __infiniteCycle__:
-    {
-      (* (_sys_.ptrArrayfn))      [ 0x01 ](  );
-    } goto                   __infiniteCycle__;
-    #undef                                value
-    #undef                                error
-    #ifndef                               error
-      goto                                 end;
-    #else
-      return                            +-0x01;
-    #endif
-  } 
-  catch                  (const char* msgError) 
-  {
-    Serial.          print("Erro detectado: ");
-    Serial.                    print(msgError);
-    Serial.                        write('\n');
-    return                              -+0x01;
-  } 
-  catch                                 ( ... ) 
-  {
-    Serial.       print("Erro desconhecido\n");
-    return                              ~~0x01;
+    (*(_sys_.funcStorage.ptrArrayfn))[1]();
+    goto __infiniteCycle__;
+
+    #undef error
+  } catch (const char* msgError) {
+    Serial.print("Erro detectado: ");
+    Serial.print(msgError);
+    Serial.write('\n');
+    return -1;
+  } catch (...) {
+    Serial.print("Erro desconhecido\n");
+    return -1;
   }
-  end: return                             0x00;
+  return 0;
 };
