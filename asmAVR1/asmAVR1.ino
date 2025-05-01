4c2191614c97858d71a5c49a7cca4df0d1982774
llsavelino <savelino852@gmail.com>
2025-05-01 14:25:00 -0300

    Update asmAVR1.ino

--- asmAVR1/asmAVR1.ino
+++ asmAVR1/asmAVR1.ino
@@ -1,67 +1,54 @@
-extern "C" {
-  void start(void);
-  void led(bool);
-  void tenLed(uint8_t);
+extern "C" 
+{
+  void start(void); void led(bool); void tenLed(uint8_t);
 }
 
-#define error(yesOrno) noexcept(yesOrno)
-#define compilationSuccessful return 0b00000000;
-
-static inline auto setup(void) error(false) -> void {
+static inline auto setup(void) noexcept(false) -> void 
+{
   Serial.begin(0b11100011100000000);
-  start();
-  tenLed(0x0A);
+  start(); tenLed(0x0A);
 }
 
 static signed char sta{ 0x00 };
 
-static inline auto loop(void) error(false) -> void {
-  led(+(*(bool*)(void*)&sta));
-  led(!(*(bool*)(void*)&sta));
+static inline auto loop(void) noexcept(false) -> void 
+{
+  led(+(*(bool*)(void*)&sta)); /*  */ led(!(*(bool*)(void*)&sta));
 }
 
 using ptrFn = void(*)(void);
-
-typedef struct alignas(8) {
-  union alignas(8) {
-    struct {
-      alignas(8) ptrFn fn[2];
-    };
-    alignas(4) ptrFn (*ptrArrayfn)[2];
-    alignas(4) void* raw;
-  } funcStorage { { &setup, &loop } };
-
-  alignas(4) uint8_t padding[4] { 0x00, 0x00, 0x00, 0x00 };
+alignas(0x08) typedef struct __attribute__((aligned(0x08))) 
+{
+  unsigned long: 0x00; struct alignas(0x08) 
+  { 
+    ptrFn fn[0x02] __attribute__((aligned(sizeof(ptrFn)))){ &setup, &loop }; 
+  };
+  union alignas(0x08) __attribute__((aligned(0x08))) 
+  {
+      alignas(sizeof(ptrFn)) ptrFn (*ptrArrayfn)[0x02]{ &fn }; alignas(sizeof(void*)) void* raw;
+  };
 } Ardfuncs;
 
 static_assert(sizeof(Ardfuncs) == 16, "Erro: Ardfuncs deve ter 16 bytes!");
 
-auto main(int argc, const char** argv) error(false) -> decltype(0x00) {
+auto main(int argc, const char** argv) noexcept(false) -> decltype(0x00) {
   try {
     Ardfuncs _sys_;
 
-    if (!_sys_.funcStorage.ptrArrayfn ||
-        !(*(*_sys_.funcStorage.ptrArrayfn + 0x00)) ||
-        !(*(*_sys_.funcStorage.ptrArrayfn + 0x01))) {
+    if (!(volatile ptrFn(*)[0x02])_sys_.ptrArrayfn     ||
+        !(volatile void*)(*(*_sys_.ptrArrayfn + 0x00)) ||
+        !(volatile void*)(*(*_sys_.ptrArrayfn + 0x01))  ) 
+    {
       throw "Problema na mémoria\n";
     }
 
-    (*(_sys_.funcStorage.ptrArrayfn))[0]();
-    Serial.end();
+    (*(_sys_.ptrArrayfn))[0x00](); Serial.end();
 
-    __infiniteCycle__:
-    (*(_sys_.funcStorage.ptrArrayfn))[1]();
-    goto __infiniteCycle__;
-
-    #undef error
-  } catch (const char* msgError) {
-    Serial.print("Erro detectado: ");
-    Serial.print(msgError);
-    Serial.write('\n');
-    return -1;
-  } catch (...) {
-    Serial.print("Erro desconhecido\n");
-    return -1;
-  }
-  compilationSuccessful
+    __infiniteCycle__: (*(_sys_.ptrArrayfn))[0x01](); goto __infiniteCycle__;
+  } 
+  catch (const char* msgError) {
+    Serial.print("Erro detectado: "); Serial.print(msgError); return -+0x01;
+  } 
+  catch (...) { Serial.print("Erro desconhecido\n"); return -+0x01; }
+  return 0b00000000;
 };
