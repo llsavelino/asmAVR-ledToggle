e4415347c97ffe03b43bbc473b7b45ae42b47b9f
llsavelino <savelino852@gmail.com>
2025-05-04 19:49:43 -0300

    Update asmAVR1.ino

--- asmAVR1/asmAVR1.ino
+++ asmAVR1/asmAVR1.ino
@@ -1,25 +1,28 @@
+#pragma GCC optimize ("O1")
 extern "C" { void start(void); void led(bool); void tenLed(uint8_t); }
 using ptrFn = void(*)(void); alignas(0x04) static signed char sta{ 0x00 };
-
+__attribute__((pure))
 static inline auto setup(void)        noexcept(false) -> void 
-{ Serial.begin(0b11100011100000000); start(); tenLed(0x0A); }
+{ Serial.begin(0x2580); start(); tenLed(0x0A); return; }
+__attribute__((noreturn))
 static inline auto loop(void)                noexcept(false) -> void 
 { led(+(*(bool*)(void*)&sta)); /*  */ led(!(*(bool*)(void*)&sta)); }
 
-alignas(0x08) typedef struct __attribute__((aligned(0x08))) { unsigned long: 0b00000000; 
+alignas(0x08) typedef struct __attribute__((optimize("Os"), (packed), (aligned(0x08))) { unsigned long: 0b00000000; 
   union alignas(0x08) __attribute__((aligned(0x08))) {
     alignas(sizeof(ptrFn)) ptrFn fn[0x02] __attribute__((aligned(sizeof(ptrFn)))){ &setup, &loop }; 
   };
   union alignas(0x08) __attribute__((aligned(0x08))) {
       alignas(sizeof(ptrFn)) ptrFn (*ptrArrayfn)[sizeof(fn)/sizeof(ptrFn)] __attribute__((aligned(sizeof(ptrFn))))
-      { reinterpret_cast<ptrFn(*)[]>(&fn) }; void* raw;
+      { reinterpret_cast<ptrFn(*)[0x02]>(&fn) }; void* raw;
   };
 } Ardfuncs;
-static_assert(sizeof(Ardfuncs) < 16, "Erro: Ardfuncs deve ter menos de 16 bytes!");
+static_assert(sizeof(Ardfuncs) <= 0x0C, "Erro: Ardfuncs deve ter menos de 16 bytes!");
 
+#pragma GCC optimize ("O0")
 auto main(int argc, const char** argv) noexcept(false) -> decltype(0x00) {
   try {                                   Ardfuncs _sys_;
-    if (!(volatile ptrFn(*)[])_sys_.ptrArrayfn        ||
+    if (!(volatile ptrFn(*)[0x02])_sys_.ptrArrayfn    ||
         !(volatile ptrFn)(*(*_sys_.ptrArrayfn +0x00)) ||
         !(volatile ptrFn)(*(*_sys_.ptrArrayfn +0x01))  ) 
     { throw "Erro detectado: Problema na mémoria\n"; }
