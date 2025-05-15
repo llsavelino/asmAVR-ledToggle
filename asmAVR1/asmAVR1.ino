221c780d581347548cb00913db249ed3ed6b5e04
Lenilson A.S <savelino852@gmail.com>
2025-05-15 15:19:47 -0300

    Update asmAVR1.ino

--- asmAVR1/asmAVR1.ino
+++ asmAVR1/asmAVR1.ino
@@ -1,53 +1,50 @@
-#pragma GCC optimize ("O1")
+#pragma GCC optimize ("O0")
 #define _y_ 0x02
 #define _x_ 0x01
-
-#include <cstdint>
-#include <cstdbool>
 
 extern "C" { [[gnu::used]] void start(void); [[gnu::used]] void led(bool); [[gnu::used]] void tenLed(uint8_t); } 
 using ptrFn = void(*)(void); alignas(0x04) static signed char i{ 0x00 };
 
-[[gnu::const]][[gnu::always_inline]] static inline auto setup(void) noexcept(false) -> void { start(); tenLed(0x0A); return; }
-__attribute__((noreturn, hot))[[gnu::always_inline]] static inline auto loop(void) noexcept(false) -> void 
-{ led(+(*(bool*)(void*) &i)); /*  */ led(!(*(bool*)(void*) &i)); }
+[[gnu::const]][[gnu::always_inline]] static inline auto setup(void) noexcept(false) 
+->                                          void { start(); tenLed(0x0A); return; }
+__attribute__((noreturn, hot))[[gnu::always_inline]] static inline auto loop(void) noexcept(false)
+->                         void { led(+(*(bool*)(void*) &i)); /*  */ led(!(*(bool*)(void*) &i)); }
+
 #if defined(_y_) && defined(_x_)
   #if _y_ < 0x02 || _y_ > 0x02 && _x_ < 0x01 || _x_ > 0x01
     #error Erro indíce menor inferior ao valor mínimo...
   #elif _y_ == 0x02 && _x_ == 0x01
-alignas(0x08) typedef struct __attribute__((optimize("O0"), (aligned(0x08))) { 
+alignas(0x08) typedef struct { 
   unsigned long: 0b00000000;
-  union alignas(0x08) __attribute__((aligned(0x08))) {
-    alignas(sizeof(ptrFn)) ptrFn fn[_y_][_x_] 
-    __attribute__((aligned(sizeof(ptrFn)))){ &setup, &loop }; 
-  };
-  union alignas(0x08) __attribute__((aligned(0x08))) {
-      alignas(sizeof(ptrFn)) ptrFn (*ptrArrayfn)[sizeof(fn)/sizeof(ptrFn)][sizeof(fn)/sizeof(fn)] 
-      __attribute__((aligned(sizeof(ptrFn)))){ reinterpret_cast<ptrFn(*)[_y_][_x_]>((void*)&fn) }; void* raw;
+  union alignas(0x08) { ptrFn fn[_y_][_x_]{ &setup, &loop }; } Box;
+  union alignas(0x08) {
+      ptrFn(*ptrArrayfn)[sizeof(fn)/sizeof(ptrFn)][sizeof(fn)/sizeof(fn)] 
+      { reinterpret_cast<ptrFn(*)[_y_][_x_]>(&Box.fn) }; 
+      void* raw;
   };
 } Ardfuncs;
   #else
     #warning Ocorreu um erro não identificado!!! Isso pode causar comportamento indefinido!!!
   #endif
-static_assert(sizeof(Ardfuncs) <= 0x0C, "Erro: Ardfuncs deve ter menos de 16 bytes!");
 #else
   #warning Ocorreu um erro não identificado!!! Isso pode causar comportamento indefinido!!!
 #endif
+
 #pragma GCC optimize ("Os")
-[[noreturn]] auto main(int argc, const char** argv) noexcept(false) -> decltype(0x00) {
+auto main(int argc, const char** argv) noexcept(false) -> decltype(0x00) {
   Ardfuncs _sys_;
   #ifndef INDEX
   #define INDEX 0x00
   if (!(volatile ptrFn(*)[_y_][_x_])_sys_.ptrArrayfn                           ||
       !(volatile ptrFn)(*((&((*_sys_.ptrArrayfn)[INDEX][INDEX]) +0x00) +0x00)) ||
       !(volatile ptrFn)(*((&((*_sys_.ptrArrayfn)[INDEX][INDEX]) +0x01) +0x00))  ) 
-  [unlikely]]                                                  { return -+0x01; }
-  #endif
+  [[unlikely]]                                                 { return -+0x01; }
   #undef INDEX
   #undef _y_
   #undef _x_
+  #endif
     
-  (*(_sys_.ptrArrayfn))[  0x00  ][  0x00  ](  );  Serial.end(  );
+  (*(_sys_.ptrArrayfn))[  0x00  ][  0x00  ](  );
   while (true) { (*(_sys_.ptrArrayfn))[  0x01  ][  0x00  ](  ); } 
   
   return 0b00000000;
